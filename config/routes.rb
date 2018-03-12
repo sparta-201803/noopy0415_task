474c9f2ced9c497ff9c4418544365034b596dcd4
noopy0415 <noopy0415@gmail.com>
2018-03-12 15:26:52 +0900

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
+  get 'tasks/edit'
+
+  get 'tasks/create'
+
+  get 'tasks/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
