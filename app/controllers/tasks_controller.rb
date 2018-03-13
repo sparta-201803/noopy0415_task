715f5d842a365c5afc7b09da66ec071c82476c5a
noopy0415 <noopy0415@gmail.com>
2018-03-13 14:10:25 +0900

    Add task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,49 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+    @task =Task.new
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    if @task.save
+      redirect_to tasks_path
+    else
+      @tasks = Task.all
+      flash.now[:alert] = @task.errors.full_messages
+      render :index
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      flash.now[:alert] = @task.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/show'
-
-  get 'tasks/edit'
-
-  get 'tasks/create'
-
-  get 'tasks/destroy'
-
+  resources :tasks
+  root "tasks#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
